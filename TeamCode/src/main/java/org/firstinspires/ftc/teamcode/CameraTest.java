1bd7ce011442e72c67cbcff72b444fab823c5725
Fall Gold <92420381+Fall-Gold@users.noreply.github.com>
2023-10-26 21:21:25 -0700

    Vision kinda works
    Just need to tune colors and area now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraTest.java
@@ -4,25 +4,47 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.vision.VisionPortal;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 
 @TeleOp
 public class CameraTest extends LinearOpMode {
-
-    private VisionPortal visionPortal;
+    private PropPipeline aPipe;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        VisionPortal.Builder builder = new VisionPortal.Builder();
-        builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
-        builder.enableLiveView(true);
-        visionPortal = builder.build();
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        WebcamName AName = hardwareMap.get(WebcamName.class, "Webcam 1");
+        OpenCvCamera autoCam = OpenCvCameraFactory.getInstance().createWebcam(AName, cameraMonitorViewId);
+        autoCam.openCameraDevice();
+        this.aPipe = new PropPipeline();
+        autoCam.setPipeline(aPipe);
+        startCamera(autoCam);
+        telemetry.addLine("waiting for start");
+        telemetry.update();
 
         waitForStart();
         while(opModeIsActive()) {
             sleep(20);
-            visionPortal.resumeStreaming();
+            telemetry.addData("x-value", aPipe.getX());
+            telemetry.addData("y-value", aPipe.getY());
+            telemetry.update();
         }
-        visionPortal.close();
+    }
+
+    public void startCamera(OpenCvCamera wcam) {
+        if (!isStopRequested()) {
+            try {
+                wcam.openCameraDevice();
+                wcam.startStreaming(640,480, OpenCvCameraRotation.UPRIGHT);
+            }
+            catch (Exception e) {
+                telemetry.addData("error",e);
+                telemetry.update();
+                startCamera(wcam);
+            }
+        }
     }
 }
